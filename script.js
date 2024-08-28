1f48e408f53b3c64588f652771895651d35c99a7
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-08-28 17:37:29 +0200

    milestone 0 (created array of of objects)

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
-console.dir('palle')
+/*# DATI
+Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+Barbara Ramos |	Graphic Designer     |
+/* Utilizzando i dati forniti, crea un array di oggetti per presentare il Team
+Ogni membro possiede: Nome, Informazioni e foto.
+
+MILESTONE 0
+Crea l'array di oggetti con le informazioni fornite.
+
+MILESTONE 1
+Stampa su console il nome, le informazioni e la stringa della foto
+
+MILESTONE 2
+Stampa le stesse informazioni sul DOM sottoforma di stringhe
+
+BONUS 1
+Trasforma la stringa in una foto effettiva
+
+BONUS 2
+Creare delle card che contengono i nomi, informazioni,e la foto.
+*/
+
+const ourTeam = [
+    {
+        name: 'Wayne Barret',
+        info: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        info: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        info: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        info: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        info: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        info: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
