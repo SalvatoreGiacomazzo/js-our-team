bf9e58322a2094b5d26f2eb2efe1b51aebb308d8
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-08-29 00:42:23 +0200

    second milestone and bonus 1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
 Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
 Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
 Scott Estrada |	Developer            |	scott-estrada-developer.jpg
-Barbara Ramos |	Graphic Designer     |
+Barbara Ramos |	Graphic Designer     |  barbara-ramos-graphic-designer.jpg
 /* Utilizzando i dati forniti, crea un array di oggetti per presentare il Team
 Ogni membro possiede: Nome, Informazioni e foto.
 
@@ -23,53 +23,78 @@ Trasforma la stringa in una foto effettiva
 BONUS 2
 Creare delle card che contengono i nomi, informazioni,e la foto.
 */
+//recupero gli elementi dal DOM
+const container = document.getElementById('container')
+
+
 //Creazione dell'array di oggetti
 const ourTeam = [
     {
-        name: 'Wayne Barret',
+        name: 'Wayne Barnett',
         info: 'Founder & CEO',
-        photo: 'wayne-barnett-founder-ceo.jpg'
+        photo: './img/wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
         info: 'Chief Editor',
-        photo: 'angela-caroll-chief-editor.jpg'
+        photo: './img/angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         info: 'Office Manager',
-        photo: 'walter-gordon-office-manager.jpg'
+        photo: './img/walter-gordon-office-manager.jpg'
     },
     {
         name: 'Angela Lopez',
         info: 'Social Media Manager',
-        photo: 'angela-lopez-social-media-manager.jpg'
+        photo: './img/angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Scott Estrada',
         info: 'Developer',
-        photo: 'scott-estrada-developer.jpg'
+        photo: './img/scott-estrada-developer.jpg'
     },
     {
         name: 'Barbara Ramos',
         info: 'Graphic Designer',
-        photo: 'barbara-ramos-graphic-designer.jpg'
-    }
+        photo: './img/barbara-ramos-graphic-designer.jpg'
+    },
+
 ];
-
-
 
 
 // Stampa in console, operazione ripetuta quindi ciclo for
 //Stampa in bracket notation
-for (teamMember of ourTeam) {
+for (const teamMember of ourTeam) {
     console.log(`Name: ${teamMember['name']}`);
     console.log(`Info: ${teamMember['info']}`);
     console.log(`Photo: ${teamMember['photo']}`);
     console.log('-------------------------'); // linea di separazione
 }
 
+// Stampa in pagina delle informazioni di ogni membro del team
+for (const teamMember of ourTeam) {
+    const memberDiv = document.createElement('div'); // Crea un div per contenere le informazioni di ogni membro
 
+
+    const photoImg = document.createElement('img'); // creo un elemento img per la foto
+    photoImg.src = teamMember.photo; // Imposta l'URL dell'immagine
+
+    const nameHeading = document.createElement('h3');   // creo un h3 per il nome
+    nameHeading.textContent = teamMember.name;
+
+
+    const infoParagraph = document.createElement('p'); // creo un elemento p per il ruolo/info
+    infoParagraph.textContent = teamMember.info;
+
+    //Stampa in pagina delle informazioni, della foto e del nome attraverso il div creato prima
+    memberDiv.appendChild(photoImg);
+    memberDiv.appendChild(nameHeading);
+    memberDiv.appendChild(infoParagraph);
+
+    // collego il div del singolo membro al container principale
+    container.appendChild(memberDiv);
+}
 
 
 
